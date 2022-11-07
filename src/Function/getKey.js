5a6afd7e1a3e41e06c20a8f7f9c6d7829c2dd0f6
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-06 22:01:27 -0500

    Update getKey.js

--- src/Function/getKey.js
+++ src/Function/getKey.js
@@ -1,7 +1,7 @@
 let idx = -1;
 
 const keys = [
-  [process.env.COMPILE_CLIENT_ID1, process.env.COMPILE_CLIENT_SECRET1],
+  ["244d5ca662a87765be0bac7f91b87f5e", "572278e4cff7c7d93d28fde95baced91bcb2b60efccff3d9328d85e18ab5bd15"],
 ];
 
 const sz = keys.length;
